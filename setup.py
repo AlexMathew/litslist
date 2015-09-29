0c1dd8b7b1d14ffd0603d62a9ddb7cc43fd6ede2
Alex Mathew <alexmathew003@gmail.com>
2015-09-29 12:08:32 +0530

    Add setup file

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+readme = open('README.md').read()
+
+setup(
+    name='litslist',
+    version='0.1',
+    description='',
+    long_description=readme,
+    author='Alex Mathew',
+    author_email='alexmathew003@gmail.com',
+    url='https://alexmathew.github.io',
+    packages=find_packages(exclude=('tests',)),
+    package_dir={'litslist':
+                 'litslist'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords=['els'],
+    entry_points={
+        'console_scripts': ['litslist = litslist.cmd.runCLI']
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+)
