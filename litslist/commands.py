f9eed267c880f3a3014742c065fa20bf342df5c9
Alex Mathew <alexmathew003@gmail.com>
2015-10-06 19:21:26 +0530

    Add implementation for writing into csv

--- litslist/commands.py
+++ litslist/commands.py
@@ -15,20 +15,26 @@ def run_create(count):
 		sets[filename[:filename.index('.')].title()] = content
 	if not os.path.exists('Lists'):
 		os.mkdir('Lists')
+	csvfile = open(os.path.join(os.curdir, 'Sets.csv'), 'w')
+	csvwriter = csv.DictWriter(csvfile, sets.keys())
+	csvwriter.writeheader()
 	break_point = count
 	for i in xrange(count):
 		try:
 			f = open(os.path.join(os.curdir, 'Lists', str(i+1) + '.txt'), 'w')
 			file_content = ''
+			csv_content = dict()
 			for category, category_list in sets.items():
 				file_content += (category + ' -- ' + category_list[i] + '\n')
+				csv_content[category] = category_list[i]
 			f.write(file_content)
+			csvwriter.writerow(csv_content)
 			f.close()
 		except IndexError:
 			break_point = i
 			break
+	csvfile.close()
 	try:
-		print sets
 		for category, category_list in sets.items():
 			f = open(os.path.join(os.curdir, 'Remaining_' + category + '.txt'), 'w')
 			file_content = "\n".join(category_list[break_point:])
